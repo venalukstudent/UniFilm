6a44c2f2d2ee78d94494546d808d758106bb7a4f
NATANIEL RATUWONGO <NATANIEL RATUWONGO@LAPTOP-3FANOUB2>
2025-11-03 11:21:53 +0800

    tambah index

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import SplashScreen from './src/pages/SplashScreen';
 import SignIn from './src/pages/SignIn';
+import SignUp from './src/pages/SignUp';
 
-AppRegistry.registerComponent(appName, () => SignIn);
+AppRegistry.registerComponent(appName, () => SignUp);
