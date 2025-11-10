1844dd0c2e8d6b69691aacb4afc8c566c9299c97
Venal <s22310263@student.unklab.ac.id>
2025-11-11 02:27:54 +0800

    run series

--- index.js
+++ index.js
@@ -7,5 +7,7 @@ import {name as appName} from './app.json';
 import SplashScreen from './src/pages/SplashScreen';
 import SignIn from './src/pages/SignIn';
 import SignUp from './src/pages/SignUp';
+import Movies from './src/pages/Movies';
+import Series from './src/pages/Series';
 
-AppRegistry.registerComponent(appName, () => SignUp);
+AppRegistry.registerComponent(appName, () => Series);
