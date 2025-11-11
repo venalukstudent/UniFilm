a7a3a4702e2e64091d557ad167849f2dcb201aa1
Andreas Maengkom <s22310312@student.unklab.ac.id>
2025-11-11 23:53:58 +0800

    add button in signin

---
+++ src/components/atoms/Button/index.tsx
@@ -0,0 +1,29 @@
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import React from 'react';
+
+const Button = ({title, color = 'rgb(77, 77, 255)', textColor = '#FFFFFF'}) => {
+  return (
+    <TouchableOpacity style={styles.button(color)} activeOpacity={0.5}>
+
+      <Text style={styles.text(textColor)}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({
+  button: color => ({
+    backgroundColor: color,
+    padding: 12,
+    borderRadius: 8,
+
+    alignItems: 'center',
+  }),
+  text: textColor => ({
+    textAlign: 'center',
+    fontFamily: 'Poppins-Medium',
+    fontSize: 20,
+    color: textColor,
+  }),
+});
