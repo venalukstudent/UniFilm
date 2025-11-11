f07a90603cf02f849a28a1ddc5f55c217b904cf8
NATANIEL RATUWONGO <NATANIEL RATUWONGO@LAPTOP-3FANOUB2>
2025-11-11 23:46:16 +0800

    movie list

---
+++ src/pages/Home/Index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const Home = ({navigation}) => {
+  // Data dummy film
+  const newMovies = [
+    {
+      id: '1',
+      title: 'ONE PIECE FILM RED',
+      rating: 8.6,
+      image: require('../../assets/onepiece.jpg'),
+    },
+    {
+      id: '2',
+      title: 'LARA ATI',
+      rating: 8.5,
+      image: require('../../assets/laraati.jpg'),
+    },
+    {
+      id: '3',
+      title: 'MIRACLE IN CELL NO.7',
+      rating: 7.5,
+      image: require('../../assets/miracle.jpg'),
+    },
+  ];
