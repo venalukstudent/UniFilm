8366af32c902a7652e4025d09581964d0b0bb9d1
NATANIEL RATUWONGO <NATANIEL RATUWONGO@LAPTOP-3FANOUB2>
2025-11-11 23:47:04 +0800

    other

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -32,3 +32,24 @@ const Home = ({navigation}) => {
       image: require('../../assets/miracle.jpg'),
     },
   ];
+
+  const otherMovies = [
+    {
+      id: '4',
+      title: 'SNOWDEN',
+      rating: 7.8,
+      image: require('../../assets/snowden.jpg'),
+    },
+    {
+      id: '5',
+      title: 'WHO AM I',
+      rating: 8.0,
+      image: require('../../assets/whoami.jpg'),
+    },
+    {
+      id: '6',
+      title: 'MR. ROBOT',
+      rating: 8.5,
+      image: require('../../assets/mrrobot.jpg'),
+    },
+  ];
