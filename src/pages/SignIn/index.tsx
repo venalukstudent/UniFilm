703d0dfe5cacb8986f51860c90dec66d0a9238ca
Andreas Maengkom <s22310312@student.unklab.ac.id>
2025-11-11 23:47:26 +0800

    add textinput in  signin

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,50 @@
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  Image,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import React, {useState} from 'react';
+import TextInput from '../../components/molecules/TextInput';
+import Button from '../../components/atoms/Button';
+import Gap from '../../components/atoms/Gap';
+import Logo from '../../assets/Logo.svg';
+
+const SignUp = ({navigation}) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onSignUp = () => {
+    console.log('Sign Up Data:', {email, password});
+  };
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        contentContainerStyle={styles.scrollContent}
+        showsVerticalScrollIndicator={false}>
+        <View style={styles.logoWrapper}>
+          <Logo width={200} height={200} />{' '}
+        </View> 
+        <Gap height={10} />
+        <Text style={styles.pageTitle}>Login to UniFilm</Text>
+        <Gap height={30} />
+        <View style={styles.formContainer}>
+          <TextInput
+            label="Email"
+            placeholder="Enter your email address"
+            value={email}
+            onChangeText={setEmail}
+            keyboardType="email-address"
+          />
+          <Gap height={16} />
+
+          <TextInput
+            label="Password"
+            placeholder="Enter your password"
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry
+          />
+          <Gap height={30} />
